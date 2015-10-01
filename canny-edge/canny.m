2e615749bc946881561ea56327e5ee74016d42d0
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-10-01 17:50:51 -0400

    Add comments and clean up

--- canny-edge/canny.m
+++ canny-edge/canny.m
@@ -1,9 +1,11 @@
-sigma = 2;
-
+%% Read the image and convert to double
 I = imread('resources/city.pgm');
 I = im2double(I);
 imshow(I);
 figure;
+
+%% Blur the image using Gaussian filter
+sigma = 2;
 G = gaussian(sigma);
 filtered = conv2(I, G,'same');
 imshow(filtered);
--- canny-edge/gaussian.m
+++ canny-edge/gaussian.m
@@ -4,5 +4,5 @@ x = floor(-4*sigma):ceil(4*sigma);
 y = floor(-4*sigma):ceil(4*sigma);
 [x, y] = meshgrid(x, y);
 
-G = 1/(2*pi*sigma^2) * exp(-(x.^2+y.^2)/(2*sigma));
+G = 1/sqrt(2*pi*sigma^2) * exp(-(x.^2+y.^2)/(2*sigma));
 G = G./sum(G(:));
