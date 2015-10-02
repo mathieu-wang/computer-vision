0ec3bbc7a0e861746877dbc7fdee27d0a47ab13f
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-10-02 00:28:16 -0400

    Find partial derivatives of smoothed image.
    Find magnitudes and orientation

--- canny-edge/canny.m
+++ canny-edge/canny.m
@@ -5,7 +5,27 @@ imshow(I);
 figure;
 
 %% Blur the image using Gaussian filter
-sigma = 2;
+sigma = 1;
 G = gaussian(sigma);
-filtered = conv2(I, G,'same');
-imshow(filtered);
+S = conv2(I, G, 'same'); % Get smoothed image S
+imshow(S);
+figure
+
+%% Get partial derivatives of the smoothed image using first-difference approximations
+Sx = S;
+Sy = S;
+[rows, columns] = size(S);
+for i = 1:rows-1
+    for j = 1:columns-1
+        Sx(i, j) = (S(i, j+1) - S(i, j) + S(i+1, j+1) - S(i+1, j))/2;
+        Sy(i, j) = (S(i, j) - S(i+1, j) + S(i, j+1) - S(i+1, j+1))/2;
+    end
+end
+
+%% Get the magnitude and orientation of the gradient
+M = sqrt(Sx.^2 + Sy.^2);
+imshow(M);
+figure
+theta = atan2(Sy, Sx);
+imshow(theta);
+figure
